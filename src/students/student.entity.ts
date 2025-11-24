4ae70690736625b4768f93522291d0cb5f9937e1
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-24 12:16:49 +0530

    student details as entity

---
+++ src/students/student.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, CreateDateColumn } from 'typeorm';
+import { Enrollment } from '../enrollments/enrollments.entity';
+
+@Entity({ name: 'students' })
+export class Student {
+  
+  @PrimaryGeneratedColumn()
+  student_id: number;
+
+  @Column({ length: 60,nullable:false })
+  name: string;
+
+  @Column({ type: 'tinyint',nullable:false })
+  age: number;
+
+  @Column({ type: 'char', length: 1 ,nullable:false })
+  gender: string;
+
+  @Column({ length: 100, nullable: true })
+  city?: string;
+
+  @Column({ type: 'date', nullable: false })
+  date_of_birth: Date;
+
+
+  @OneToMany(() => Enrollment, (enr: Enrollment) => enr.students)
+enrollments: Enrollment[];
+
+}
