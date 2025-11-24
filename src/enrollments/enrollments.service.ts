f37c43939f2162d996db4338821fcc7abec0de50
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-24 12:20:42 +0530

    enrollments service  logic  file

---
+++ src/enrollments/enrollments.service.ts
@@ -0,0 +1,38 @@
+ import { Injectable, NotFoundException } from '@nestjs/common';
+ import { Enrollment } from './enrollments.entity';
+ import { InjectRepository } from '@nestjs/typeorm';
+ import { Repository } from 'typeorm';
+ import { Student } from '../students/student.entity';
+
+ @Injectable()
+ export class EnrollmentsService {
+       constructor(
+      @InjectRepository(Enrollment) private epo: Repository<Enrollment>,
+      @InjectRepository(Student) private readonly repo: Repository<Student>,
+   ) {}
+
+    
+    async create(enrollment: Partial<Enrollment>): Promise<Student> {
+     const { students } = enrollment;
+    
+    return new  Student;
+ 
+    }
+
+   findAll() {
+     return this.epo.find({ relations: ['student'] });
+   }
+
+   findOne(id: number) {
+     return this.epo.findOne({ where: { enrollment_id: id }, relations: ['student'] });
+   }
+
+   update(id: number, patch: Partial<Enrollment>) {
+     return this.epo.save({ ...patch, student_id: id });
+   }
+
+   remove(id: number) {
+     return this.epo.delete({ enrollment_id: id });
+   }
+ }
+
