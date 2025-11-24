97b6b57ab8d5757ea242ae3328644b801d11c84b
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-24 12:12:12 +0530

    app root file

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StudentsModule } from './students/students.module';
+import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
+import { Student } from './students/student.entity';
+import { Course } from './courses/courses.entity';
+import { Enrollment } from './enrollments/enrollments.entity';
+
+@Module({
+  imports: [
+     
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Student, Course, Enrollment],
+        synchronize: false, 
+      }),
+      inject: [ConfigService],
+    }),
+    StudentsModule,
+    CoursesModule,
+    EnrollmentsModule,
+  ],
+})
+export class AppModule {}
