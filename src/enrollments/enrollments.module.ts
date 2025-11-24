650fad0761b87ecc29ac94123068eee04236e11e
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-24 12:21:41 +0530

    enrollments root file

---
+++ src/enrollments/enrollments.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { EnrollmentsController } from './enrollments.controller';
+import { EnrollmentsService } from './enrollments.service';
+import { Enrollment } from './enrollments.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from 'src/students/student.entity';
+
+@Module({
+
+   imports: [
+   
+    TypeOrmModule.forFeature([Student,Enrollment])],
+
+  controllers: [EnrollmentsController],
+  providers: [EnrollmentsService]
+})
+export class EnrollmentsModule {}
