be2496b5f00415edf1b0cabc560c1ae86ad10bfa
Sankar <sankar.sivaperumal@datasirpi.com>
2025-11-25 12:11:13 +0530

    update the app module

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
+
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-    app.use(bodyParser.urlencoded({ extended: true }));
-  // JSON parsing is already enabled by default
-  app.use(bodyParser.json());
-
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true, 
