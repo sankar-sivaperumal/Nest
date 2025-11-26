bd11f5e29a1521cf3621cdf4cad68d37e5d4cecb
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-26 17:27:51 +0530

    update a field files

--- src/students/student.entity.ts
+++ src/students/student.entity.ts
@@ -23,11 +23,10 @@ export class Student {
   date_of_birth: Date;
 
 
-  // @Column('simple-array', { nullable: true })
-  // files: string[];
-
+  @Column('simple-array', { nullable: true })
+  files: string[];
 
   @OneToMany(() => Enrollment, (enr: Enrollment) => enr.students)
-enrollments: Enrollment[];
+  enrollments: Enrollment[];
 
 }
