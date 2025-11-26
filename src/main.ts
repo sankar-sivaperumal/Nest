bdd8528e2e660aa25a1b4af8342cd4f1073ff94a
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-26 09:23:29 +0530

    updated main file

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
+   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true, 
-   
   }));
   await app.listen(process.env.PORT ?? 3000);
 }
