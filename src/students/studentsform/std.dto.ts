607a5c432635450382c354a5324137769c1fe60c
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-26 10:07:57 +0530

    form data dto file

--- src/students/studentsform/std.dto.ts
+++ src/students/studentsform/std.dto.ts
@@ -1,79 +1,44 @@
 
-// import { Type } from "class-transformer";
-// import { IsInt, IsNotEmpty, IsString, IsDateString, IsOptional, IsDecimal } from "class-validator";
-
-
-// export class stddto {
-
-//     @IsString()
-//     name: string;
-
-//     @Type(() => Number)
-//     @IsInt()
-//     age: number;
-
-//     @IsString()
-//     gender: string;
-
-//     @IsOptional()
-//     @IsString()
-//     city: string;
-
-   
-//     @IsDateString()
-//     date_of_birth: string; 
-
-//     @Type(() => Number)
-//     @IsOptional()
-//     @IsInt()
-//     course_id: number;
-
-//     @IsOptional()
-//     @IsString()
-//     course_name: string;
-
-//     @IsOptional()
-//     @IsString()
-//     teacher_name: string;
-
-//     @Type(() => Number)
-//     @IsOptional()
-//     @IsDecimal()
-//     marks: number;
-   
-
-
-// }
-
-
-// src/std.dto.ts (Recommended)
-
 import { Type } from "class-transformer";
-import { IsInt, IsNotEmpty, IsString, IsOptional, Min } from "class-validator";
+import { IsInt, IsNotEmpty, IsString, IsDateString, IsOptional, IsDecimal } from "class-validator";
+
 
 export class stddto {
 
-    @IsNotEmpty({ message: 'name should not be empty' })
-    @IsString({ message: 'name must be a string' })
+    @IsString()
     name: string;
 
     @Type(() => Number)
-    @IsInt({ message: 'age must be an integer number' })
-    @Min(0)
-    @IsNotEmpty({ message: 'age should not be empty' })
+    @IsInt()
     age: number;
 
-    @IsNotEmpty({ message: 'gender should not be empty' })
-    @IsString({ message: 'gender must be a string' })
+    @IsString()
     gender: string;
 
     @IsOptional()
     @IsString()
-    city?: string; 
+    city: string;
 
-    @IsNotEmpty({ message: 'date_of_birth should not be empty' })
-    @IsString() 
-
+   
+    @IsDateString()
     date_of_birth: string; 
-    
-}
+
+    @Type(() => Number)
+    @IsOptional()
+    @IsInt()
+    course_id: number;
+
+    @IsOptional()
+    @IsString()
+    course_name: string;
+
+    @IsOptional()
+    @IsString()
+    teacher_name: string;
+
+
+    @IsOptional()
+    @IsDecimal()    
+    marks: number;
+ }
+
