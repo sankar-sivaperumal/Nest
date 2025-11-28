fdc5b96a66fd7ac96cb27c2bd312186d8f5709a9
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-11-28 18:21:10 +0530

    created new post method

--- src/Users/user.module.ts
+++ src/Users/user.module.ts
@@ -2,9 +2,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { StudentsModule } from 'src/students/students.module';
+
 
 @Module({
+  imports: [StudentsModule],
   controllers: [UserController],
   providers: [UserService],
 })
+
+
 export class UserModule {}
