4b755d773ecacf63c59d23de34aef9c65d6b734d
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-01 10:36:24 +0530

    update enrollments class and student to post all values

--- src/Users/user.service.ts
+++ src/Users/user.service.ts
@@ -4,94 +4,41 @@ import { StudentDto } from 'src/students/dto/student.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from 'src/students/student.entity';
+import { Enrollment } from 'src/enrollments/enrollments.entity';
 
-@Injectable()
-export class UserService {
-    constructor(
-      @InjectRepository(Student)
-      private userrepo: Repository<Student>,
-    ) {}
-  private user(input: any): userDto {
-    const dto = new userDto();
-    dto.name = input.name;
-    dto.age = input.age;
-    dto.gender = input.gender;
-    dto.date_of_birth = input.date_of_birth;
-    dto.city = input.city;
-    dto.course_id = input.course_id;
-    dto.marks = input.marks;
-    dto.files = input.files;
-    return dto;
-  }
-
-  // async createUser(input: any, files: Express.Multer.File[]): Promise<userDto> {
-  //   const createdUserDto = this.user(input);
-  //   console.log(createdUserDto);
-  //   return createdUserDto
-
-
-   async createUser(input: any, files: Express.Multer.File[]): Promise<userDto> {
-  const student = this.userrepo.create({ ...input, files });
-  const savedStudent = await this.userrepo.save(student);
-  return this.createUser(savedStudent,files);
-}
-  }
-
- 
-
-
-/* @Injectable()
+ @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(Student)
-    private userrepo: Repository<Student>,
-  ) {}
-
-  async createUser(input: userDto, files: Express.Multer.File[]) {
   
-    const fileNames = files?.map(f => f.filename) ?? [];
-
-      const userEntity = this.userrepo.create({
-      ...input,
-      files: fileNames, 
-    });
-
-      const savedUser = await this.userrepo.save(userEntity);
-
-    return savedUser; 
-  }
-}
- */
-
-
-/* import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Student } from '../students/student.entity';
-import { StudentDto } from '../students/dto/student.dto';
-
-@Injectable()
-export class UserService {
-  constructor(
-    @InjectRepository(Student)
-    private readonly studentRepository: Repository<Student>,
+    private userrepo: Repository<Student>,
+      @InjectRepository(Enrollment) private epo: Repository<Enrollment>,
   ) {}
 
-  async createUser(
-    input: StudentDto,
-    file?: Express.Multer.File[],
-  ): Promise<Student> {
-    const student = this.studentRepository.create({
-      ...input,
-      file: file?.map((file) => ({
-        filename: file.originalname,
-        path: file.path,
-      })),
+
+async createUser(input: userDto, files: Express.Multer.File[]) {
+  const fileNames = files?.map(f => f.filename) ?? [];
+
+  const student = this.userrepo.create({
+    ...input,
+    files: fileNames,
+  });
+
+  const savedStudent = await this.userrepo.save(student);
+
+   if (input.marks) {
+    const enrollment = this.epo.create({
+      marks: input.marks,
+      students: savedStudent 
     });
 
-    const savedStudent = await this.studentRepository.save(student);
-
-    return savedStudent; 
+    await this.epo.save(enrollment);
   }
+
+
+  return this.userrepo.findOne({
+    where: { student_id: savedStudent.student_id },
+    relations: ['enrollments']
+  });
 }
- */
+}
