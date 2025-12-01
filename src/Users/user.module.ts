1cee27175699394a5defa6f4447a69fe4e3e3360
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-01 10:35:42 +0530

    update enrollments modules

--- src/Users/user.module.ts
+++ src/Users/user.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { StudentsModule } from 'src/students/students.module';
+import { EnrollmentsModule } from 'src/enrollments/enrollments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [StudentsModule],
+  imports: [StudentsModule,EnrollmentsModule],
   controllers: [UserController],
   providers: [UserService],
 })
