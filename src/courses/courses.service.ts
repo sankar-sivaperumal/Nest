6a3c519a93909a9384613a3a42817f96c6133a01
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-15 15:51:35 +0530

    Updated link for studnets and enrollments

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -4,40 +4,41 @@ import { Enrollment } from 'src/enrollments/enrollments.entity';
 import { Student } from 'src/students/student.entity';
 import { Repository } from 'typeorm';
 import { Course } from './courses.entity';
-
-@Injectable()
+ @Injectable()
 export class CoursesService {
     
     constructor(
-      @InjectRepository(Course) private cro:Repository<Course>,
+      @InjectRepository(Course) private cro: Repository<Course>,
       @InjectRepository(Enrollment) private epo: Repository<Enrollment>,
       @InjectRepository(Student) private readonly repo: Repository<Student>,
    ) {}
 
-    
-    async create(Course: Partial<Course>): Promise<Student> {
-     const { course_id } = Course;
-    
-    return new  Student;
+   async create(course: Partial<Course>): Promise<Course> {
+   
+     const newCourse = this.cro.create(course);
+     return this.cro.save(newCourse);
+   }
+
  
-    }
-
-   findAll() {
-     return this.cro.find({ relations: ['student'] });
+   async findAll() {
+     return this.cro.find({
+       relations: ['enrollments', 'enrollments.students'],
+     });
    }
 
-   findOne(id: number) {
-     return this.cro.findOne({ where: { course_id: id }, relations: ['student'] });
+  
+   async findOne(id: number) {
+     return this.cro.findOne({
+       where: { course_id: id },
+       relations: ['enrollments', 'enrollments.students'], 
+     });
    }
 
-   update(id: number, patch: Partial<Course>) {
+   async update(id: number, patch: Partial<Course>) {
      return this.cro.save({ ...patch, course_id: id });
    }
 
-   remove(id: number) {
+   async remove(id: number) {
      return this.cro.delete({ course_id: id });
    }
- }
-
-
-
+}
