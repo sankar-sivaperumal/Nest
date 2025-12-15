99d0978958bc64cb16226a59733c5223a2b0567e
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-15 13:01:02 +0530

    remove unwanted imports

--- src/enrollments/enrollments.entity.ts
+++ src/enrollments/enrollments.entity.ts
@@ -1,27 +1,20 @@
-import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
-import { Student} from '../students/student.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Student } from '../students/student.entity';
 import { Course } from '../courses/courses.entity';
 
 @Entity({ name: 'enrollments' })
 export class Enrollment {
-  static push(arg0: { enrollment_id: any; marks: any; course_id: any; }) {
-    throw new Error('Method not implemented.');
-  }
- @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn()
   enrollment_id: number;
 
   @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true })
   marks: number;
 
-  @ManyToOne(() => Student, (students) => students.enrollments, { onDelete: 'CASCADE' })
-  @JoinColumn({ name: 'student_id' })
+  @ManyToOne(() => Student, (student) => student.enrollments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'student_id' })  
   students: Student;
 
-  @ManyToOne(() => Course, (courses) => courses.enrollments, { onDelete: 'CASCADE' })
-  @JoinColumn({ name: 'course_id' })
+  @ManyToOne(() => Course, (course) => course.enrollments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'course_id' })  
   courses: Course;
-
-
-
-
 }
