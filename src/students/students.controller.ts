84e578ba63c4a30f53bbbe6796f7f2cf5ffbf460
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-18 13:26:00 +0530

    updated filter,sort

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -3,19 +3,31 @@ import { StudentsService } from './students.service';
 import { StudentDto } from './dto/student.dto';
 import { updatestd } from './dto/patch.dto'
 import type { Response } from 'express';
+import { Student } from './student.entity';
 
 @Controller('students')
 export class StudentsController {
   constructor(  private readonly svc: StudentsService){}
    
   // Get All Students Record
-@Get()
-async findAll(
-  @Query('page') page = 1,
-  @Query('limit') limit = 10,
-) {
-  return await this.svc.findAll(+page, +limit);
-}
+  @Get()
+  findAll(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query() query: any
+  ) {
+    const { sortField, sortOrder = "asc", name, age, gender, city } = query;
+
+    const filters = { name, age, gender, city };
+
+    return this.svc.findAll(
+      Number(page),
+      Number(limit),
+      sortField,
+      sortOrder,
+      filters
+    );
+  }
 
 
   // Get By Gender
