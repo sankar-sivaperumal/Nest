d9c3d09a255472300292e83796c2acc673c89a57
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-23 16:08:31 +0530

    updated  main file with cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; 
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.enableCors();
-   app.useGlobalPipes(new ValidationPipe({
+
+
+  app.enableCors({
+    origin: 'http://localhost:5173',  
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true, 
+  });
+
+  // Global validation
+  app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true, 
   }));
-  await app.listen(process.env.PORT ?? 5000);
+
+  await app.listen(5000);
 }
 bootstrap();
